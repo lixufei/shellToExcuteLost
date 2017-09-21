55cc867a06be7838e375a7e283f03fda322d9769
LiXuFei <xfli@thoughtworks.com>
2017-09-21 10:12:35 +0800

    dont count.

--- generateSql.sh
+++ generateSql.sh
@@ -3,9 +3,6 @@
 echo "start to find owner of lost sales and unsuccessful lead......"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mongo lead-management --quiet "$DIR/count.js" > count.json || { echo "count failed"; exit 1;}
-echo "count success!"
-
 mongo lead-management --quiet "$DIR/generate_update_task_for_lost_and_successful.js" > generate_update_task_for_lost_and_unsuccessful.sql || { echo "write generate_update_task_for_lost_and_unsuccessful.sql failed!"; exit 1;}
 echo "write generate_update_task_for_lost_and_unsuccessful.sql success!"
 
